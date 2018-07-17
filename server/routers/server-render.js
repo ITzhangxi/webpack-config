5f74f8d0152366f803ca41fe5be44bc00bd019ab
xizi <zackxizi@qq.com>
2018-07-17 10:45:00 +0800

    添加服务端设置mate内容

--- server/routers/server-render.js
+++ server/routers/server-render.js
@@ -9,11 +9,15 @@ module.exports = async (ctx, renderer, template) => {
   try {
     // context渲染为字符串
     const appString = await renderer.renderToString(context)
+    // 获取mate内容
+    const {title} = context.meta.inject()
     // ejs 渲染模板
     const html = ejs.render(template, {
       appString,
       style: context.renderStyles(),
-      scripts: context.renderScripts()
+      scripts: context.renderScripts(),
+      // 这里呈现的就是vue组件中设置的title 这是带有title和text的标签
+      title: title.text()
     })
     ctx.body = html
   } catch (err) {
